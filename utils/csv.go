43605d2353f20fb5303264049f940be5a76fefb0
Pedro H. Victorino <pedro.victorino@gmail.com>
2021-02-16 20:55:52 -0400

    made validations

--- utils/csv.go
+++ utils/csv.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"encoding/csv"
+	"errors"
+	"fmt"
 	"net/http"
 	"strconv"
 )
@@ -15,20 +17,49 @@ func CsvTransformer(response http.ResponseWriter, request *http.Request) [][]int
 		http.Error(response, "Error to convert CSV", http.StatusInternalServerError)
 	}
 
-	intMatrix := convertValuesFromStringToInt(result)
+	intMatrix, convertError := convertValuesFromStringToInt(result)
+
+	if convertError != nil {
+		response.WriteHeader(http.StatusBadRequest)
+		response.Write([]byte(convertError.Error()))
+	}
 
 	return intMatrix
 }
 
-func convertValuesFromStringToInt(matrix [][]string) [][]int {
+func convertValuesFromStringToInt(matrix [][]string) ([][]int, error) {
+	squareError := validateSquareMatrix(matrix)
+
+	if squareError != nil {
+		return nil, squareError
+	}
+
 	newMatrix := make([][]int, len(matrix))
 	InitializeRowsOfSlice(newMatrix)
 
 	for indexRow, row := range matrix {
 		for indexColumn, column := range row {
-			result, _ := strconv.Atoi(column)
+			result, error := strconv.Atoi(column)
+			if (error) != nil {
+				return nil, fmt.Errorf("Cannot cast element of the matrix to int: %v", error)
+			}
 			newMatrix[indexRow][indexColumn] = result
 		}
 	}
-	return newMatrix
+	return newMatrix, nil
+}
+
+func validateSquareMatrix(matrix [][]string) error {
+	var lengthRow, lengthColumn int
+	for _, row := range matrix {
+		lengthRow = len(row)
+		for _, column := range row {
+			lengthColumn = len(column)
+		}
+	}
+
+	if lengthColumn != lengthRow {
+		return errors.New("Matrix must be square: same length of rows and columns")
+	}
+	return nil
 }
